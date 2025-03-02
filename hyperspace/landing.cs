6d3f4146be9001000541583a47c5f87cd951f7cd
VulpusGenius <69985070+UrsaMiner@users.noreply.github.com>
2025-03-02 15:50:12 +0000

    Delete hyperspace/landing.cs
    moving landing code to star, which will be renamed soon

--- hyperspace/landing.cs
+++
@@ -1,7 +0,0 @@
-public class Landing{
-    public Landing(string inpname, int inpshipyard, int inpoutfitter){
-        string name = inpname
-        int shipyard = inpshipyard
-        int outfitter = inpoutfitter
-    }
-}
