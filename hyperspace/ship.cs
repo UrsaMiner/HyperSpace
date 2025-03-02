5cd357b31c4b763dffcc0c832b5cd659fa399cb3
VulpusGenius <69985070+UrsaMiner@users.noreply.github.com>
2025-03-02 15:41:23 +0000

    Update ship.cs
    fixed Ship constructor, and added back allships in C#

--- hyperspace/ship.cs
+++ hyperspace/ship.cs
@@ -1,17 +1,18 @@
 public class Ship{
-    public Ship(int inpspace) => public int[2] space = [inpspace, inpspace]; //space left, total space
-    public Ship(int inpweapon) => public int[2] weapon = [inpweapon, inpweapon]; //slots left, total slots
-    public Ship(int inpcost) => public int cost = inpcost;
-    public Ship(string inpcategory) => public string name = public string category = inpcategory;
-    public Ship(int inpmass) => public int mass = inpmass;
-    public int engine = 1; //1 is has space, 0 is doesnt
-    public List<Outfit> outfits = [];
+    public Ship(int inpspace, int inpweapon, int inpcost, string inpcategory, int inpmass){
+        protected int[2] space = [inpspace, inpspace]; //space left, total space
+        protected int[2] weapon = [inpweapon, inpweapon]; //slots left, total slots
+        protected int cost = inpcost;
+        protected string name = protected string category = inpcategory;
+        protected int mass = inpmass;
+        protected int engine = 1; //1 is has space, 0 is doesnt
+        protected List<Outfit> outfits = [];
+    }
 }
 
 
-/*
-allships = []
 
-scouter = Ship("Scouter","Scouter",25,50,1,50000)
-allships.append(scouter)
-*/
+public void defineShips(){
+    public List<Ship> allships = new List<Ship>();
+    allships.Add(Ship("Scouter",25,50,1,50000));
+}
