091ec50ac543ad379abf0ddba618a6f6fb556925
VulpusGenius <69985070+UrsaMiner@users.noreply.github.com>
2025-03-02 15:37:14 +0000

    Update outfit.cs
    [working?] version of outfit in c#

--- hyperspace/outfit.cs
+++ hyperspace/outfit.cs
@@ -1,44 +1,31 @@
-class Outfit(){
-    def __init__(self, category, name, space, cost):
-        self.category = category # integer category (0-2, 0 is default, 1 is weapon, 2 is engine)
-        self.name = name # string "name"
-        self.space = space # integer space
-        self.cost = cost # integer cost
+public class Outfit{
+    Outfit(int inpcategory, string inpname, int inpspace, int inpcost){
+        protected int category = inpcategory; //(0-2, 0 is default, 1 is weapon, 2 is engine)
+        protected string name = inpname;
+        protected int space = inpspace;
+        protected int cost = inpcost;
+    }
 }
 
 
 
-class Engine(Outfit){
-    def __init__(self, name, space, cost):
-        self.name = name
-        self.space = space
-        self.cost = cost
-        super().__init__(2,self.name,self.space,self.cost)
+public class Engine : Outfit{
+    Engine(string inpname, int inpspace, int inpcost, int inpthrust) :
+    base(2, inpname inpspace, inpcost){
+        protected int thrust = inpthrust;
+    }
 }
 
 
 
-class Weapon(Outfit){
-    def __init__(self, name, space, cost):
-        self.name = name
-        self.space = space
-        self.cost = cost
-        super().__init__(1,self.name,self.space,self.cost)
+public class Weapon : Outfit{
+    Weapon(string inpname, int inpspace, int inpcost, int inppower) :
+    base(1, inpname, inpspace, inpcost){
+        protected int power = inppower;
+    }
 }
 
-
-
-class General(Outfit){
-    def __init__(self, name, space, cost):
-        self.name = name
-        self.space = space
-        self.cost = cost
-        super().__init__(0,self.name,self.space,self.cost)
+public void defineOutfits(){
+    public List<Outfit> alloutfits = new List<Outfit>();
+    alloutfits.Add(Engine("Light Engine",10,2000,5));
 }
-
-
-
-alloutfits = []
-
-engine_light = Engine("Light Engine",10,1000)
-alloutfits.append(engine_light)
