4a3a768aaf2a71770884c15310765cedf44bca8f
VulpusGenius <69985070+UrsaMiner@users.noreply.github.com>
2025-03-02 19:58:30 +0000

    Final C# conversion

--- hyperspace/player.cs
+++ hyperspace/player.cs
@@ -4,10 +4,13 @@ public class Player{
         ulong money = 100000L;
         Star currstar = allstars[0];
         Landing currlanding = currstar.landings[0];
+        Faction currzone = allfactions[0];
         string name = input("Name: ");
     }
+    
         void doAction(){
-            string action = input("\nWhat would you like to do?\n").ToLower();
+            Console.WriteLine("\nWhat would you like to do?");
+            string action = Console.ReadLine().ToLower();
             switch (action){
                 case "sell ship":
                     sellShip();
@@ -37,6 +40,7 @@ public class Player{
                     services();
             }
         }
+    
     void sellShip(){
         if (ship != null){
             money += ship.cost * 0.5;
@@ -44,6 +48,7 @@ public class Player{
             ship = null;
         }
     }
+    
     string grammarN(string name){
         if (name[0] in ["a","e","i","o","u"])
             return string "n";
@@ -51,8 +56,10 @@ public class Player{
             return string "";
         }
     }
+
     void sellOutfit(){
-        outfitname = input("Which outfit do you want to sell?\n").ToLower();
+        Console.WriteLine("Which outfit do you want to sell?\n");
+        string outfitname = Console.ReadLine().ToLower();
         success = 0;
         foreach (Outfit curroutfit in ship.outfits){
             if (curroutfit.name.ToLower() == outfitname){
@@ -80,91 +87,102 @@ public class Player{
     }
 
     void buyShip(){
-        success = 0
-        shipname = input("Which ship do you want to buy?\n").ToLower()
-        for i in range(len(allships)):
-            if allships[i].name.ToLower() == shipname:
-                shiptobuy = allships[i]
-                success = 1
-        if success:
-            if shiptobuy.cost <= money:
-                money -= shiptobuy.cost
-                ship = shiptobuy
-                grammarn = ""
-                if shiptobuy.name[0] in ["a","e","i","o","u"]:
-                    grammarn = "n"
-                Console.WriteLine("You have bought a" + grammarn + " " + shiptobuy.name + ".")
-            else:
-                Console.WriteLine("You don't have enough money.")
-        else:
-            Console.WriteLine("That ship doesn't exist.")
+        if (currlanding != null && currlanding.shipyard){
+            Console.WriteLine("Which ship do you want to buy?\n");
+            success = 0;
+            string shipname = Console.ReadLine.ToLower();
+            foreach (Ship cycleship in currzone.ships){
+                if (cycleship.name.ToLower() == shipname){
+                    shiptobuy = allships[i];
+                    success = 1;
+                }
+            }
+            if (success){
+                if (shiptobuy.cost <= money){
+                    money -= shiptobuy.cost;
+                    ship = shiptobuy;
+                    Console.WriteLine("You have bought a" + grammarN(shiptobuy.name) + " " + shiptobuy.name + ".");
+                } else
+                    Console.WriteLine("You don't have enough money.");
+            } else
+                Console.WriteLine("You can't buy that here");
+        } else
+            Console.WriteLine("You can't do that here");
     }
-
     void buyOutfit(){
-        outfitname = input("Which outfit do you want to buy?\n").ToLower()
-        success = 0
-        for i in range(len(alloutfits)):
-            if alloutfits[i].name.ToLower() == outfitname:
-                outfittobuy = alloutfits[i]
-                success = 1
-        if success:
-            bought = 0
-            if outfittobuy.cost <= money:
-                if ship != None:
-                    if ship.space[0] >= outfittobuy.space:
-                        if outfittobuy.category == 1:
-                            if ship.weapon[0]:
-                                money -= outfittobuy.cost
-                                ship.outfits.append(outfittobuy)
-                                ship.space[0] -= outfittobuy.space
-                                ship.weapon[0] -= 1
-                                bought = 1
-                            else:
-                                Console.WriteLine("You don't have a free weapon slot.")
-                        else if outfittobuy.category == 2:
-                            if ship.engine:
-                                money -= outfittobuy.cost
-                                ship.outfits.append(outfittobuy)
-                                ship.space[0] -= outfittobuy.space
-                                ship.engine = 0
-                                bought = 1
-                            else:
-                                Console.WriteLine("You already have an engine installed.")
-                        else if outfittobuy.category == 0:
-                            money -= outfittobuy.cost
-                            ship.outfits.append(outfittobuy)
-                            ship.space[0] -= outfittobuy.space
-                            bought = 1
-                    else:
-                        Console.WriteLine("You don't have enough space.")
-                else:
-                    Console.WriteLine("You don't have a ship.")
-            else:
-                Console.WriteLine("You don't have enough money.")
-        else:
-            Console.WriteLine("That outfit doesn't exist.")
-        if bought:
-            grammarn = ""
-            if outfittobuy.name[0] in ["a","e","i","o","u"]:
-                grammarn = "n"
-            Console.WriteLine("You have bought a" + grammarn + " " + outfittobuy.name + ".")
+        if (currlanding != null && currlanding.outfitter){
+            Console.WriteLine("Which outfit do you want to buy?\n");
+            string outfitname = console.ReadLine().ToLower()
+            success = 0
+            foreach (Outfit cycleoutfit in currzone.outfits){
+                if (cycleoutfit.name.ToLower() == outfitname)
+                    outfittobuy = cycleoutfit;
+                    success = 1;
+                }
+            }
+            if (success){
+                bought = 0;
+                if (outfittobuy.cost <= money){
+                    if (ship != null){
+                        if (ship.space[0] >= outfittobuy.space){
+                            if (outfittobuy.category == 1){
+                                if (ship.weapon[0]){
+                                    money -= outfittobuy.cost;
+                                    ship.outfits.Add(outfittobuy);
+                                    ship.space[0] -= outfittobuy.space;
+                                    ship.weapon[0] -= 1;
+                                    bought = 1;
+                                } else
+                                    Console.WriteLine("You don't have a free weapon slot.");
+                            } else if (outfittobuy.category == 2){
+                                if (ship.engine){
+                                    money -= outfittobuy.cost;
+                                    ship.outfits.Add(outfittobuy);
+                                    ship.space[0] -= outfittobuy.space;
+                                    ship.engine = 0;
+                                    bought = 1;
+                                } else
+                                    Console.WriteLine("You already have an engine installed.");
+                            } else if (outfittobuy.category == 0){
+                                money -= outfittobuy.cost;
+                                ship.outfits.Add(outfittobuy);
+                                ship.space[0] -= outfittobuy.space;
+                                bought = 1;
+                                }
+                            } else
+                            Console.WriteLine("You don't have enough space.");
+                    } else
+                        Console.WriteLine("You don't have a ship.");
+                } else
+                    Console.WriteLine("You don't have enough money.");
+            } else
+                Console.WriteLine("You can't buy that here.");
+            if bought:
+                grammarn = ""
+                if outfittobuy.name[0] in ["a","e","i","o","u"]:
+                    grammarn = "n"
+                Console.WriteLine("You have bought a" + grammarn + " " + outfittobuy.name + ".")
+        } else
+            Console.WriteLine("You can't do that here.");
     }
 
     void renameShip(){
         if (ship != null)
-            ship.name = input("New Ship Name: ");
+            Console.WriteLine("What should her new name be?\n");
+            ship.name = Console.ReadLine();
         else
             Console.WriteLine("You don't have a ship to rename.");
     }
 
     void land(){
         if (currlanding == null){
-            int success = 0
-            landingname = input("Which landing do you want to land on?\n").ToLower()
-            foreach(cyclestar in currstar.landings){
-                if (cyclestar.name.ToLower() == landingname){
-                    toland = cyclestar
-                    success = 1
+            int success = 0;
+            Console.WriteLine("Which landing do you want to land on?\n");
+            landingname = Console.ReadLine().ToLower();
+            foreach(Landing cyclelanding in currstar.landings){
+                if (cyclelanding.name.ToLower() == landingname){
+                    toland = cyclelanding;
+                    success = 1;
                 }
             }
             if (success){
@@ -173,22 +191,23 @@ public class Player{
             } else
                 Console.WriteLine("You can't land there right now.");
         } else
-            Console.WriteLine("You can't do that right now.");
+            Console.WriteLine("You can't do that here.");
     }
 
     void depart(){
         if (currlanding != null){
-            Console.WriteLine("You have departed from " + location.name + ".");
+            Console.WriteLine("You have departed from " + currlanding.name + ".");
             currlanding = null;
         } else
-            Console.WriteLine("You can't do that right now.");
+            Console.WriteLine("You can't do that here.");
     }
 
     void jump(){
-        if isinstance(location, Star):
-            starname = input("Where would you like to jump to?\n").ToLower()
-            success = 0
-            foreach(Faction cyclefaction in allfactions{
+        if (currlanding == null){
+            Console.WriteLine("Where would you like to jump to?\n");
+            starname = Console.ReadLine().ToLower();
+            success = 0;
+            foreach(Faction cyclefaction in allfactions){
                 foreach(Star cyclestar in cyclefaction.stars){
                     if (cyclestar.name.ToLower() == starname){
                         success = 1;
@@ -196,7 +215,7 @@ public class Player{
                     }
                 }
             }
-            if success:
+            if (success){
                 /*
                 distx = jump.coordinates[0] - location.coordinates[0]
                 disty = jump.coordinates[1] - location.coordinates[1]
@@ -205,15 +224,16 @@ public class Player{
                 */
                 currstar = destination;
                 Console.WriteLine("You have jumped to " + location.name + ".");
-            else:
+            } else
                 Console.WriteLine("You can't jump there right now.");
-        else:
-            Console.WriteLine("You can't do that right now.");
+        }else
+            Console.WriteLine("You can't do that here.");
     }
 
     void reputation(){
-        facname = input("Which faction would you like to view?\n").ToLower()
-        success = 0
+        Console.WriteLine("Which faction would you like to view?\n");
+        facname = Console.ReadLine().ToLower();
+        success = 0;
         foreach (Faction cyclefaction in allfactions){
             if (cyclefaction.name.ToLower() == facname){
                 success = 1;
@@ -254,10 +274,9 @@ public class Player{
                 Console.WriteLine("OUTFITS:");
             }
             foreach (Outfit cycleoutfit in ship.outfits)
-                Console.WriteLine(cycleoutfit.name);
+                Console.WriteLine(cycleoutfit.name + ": Cost: " + ToString(cycleoutfit.cost) + " Space: " + ToString(cycleoutfit.space));
         }
     }
-
 
     void services()
         if (currlanding != null){
@@ -273,7 +292,8 @@ public class Player{
             if (hasnothing)
                 Console.WriteLine("This Landing has no services.");
         else
-            Console.WriteLine("You can't do that right now.");
+            Console.WriteLine("You can't do that here.");
+            
     void help()
         Console.WriteLine("COMMANDS:");
         Console.WriteLine("Buy Ship (Req. Shipyard)\nSell Ship (Req. Shipyard)");
